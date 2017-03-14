6eff9052bd9ed9401eaff55c55fc615be532f84f
David <david.sattler.biz@gmail.com>
2017-03-14 16:26:35 +0100

    add project Task2 (lesson 3) with final changes

--- tasks/Task1/Task1/Program.cs
+++ tasks/Task1/Task1/Program.cs
@@ -3,25 +3,54 @@ using System.Collections.Generic;
 
 namespace Task1
 {
+
+	// A person.
+	class Person
+	{
+		public string Name { get; }
+		public DateTime DateOfBirth { get; }
+		public bool HasBirthday
+		{
+			get
+			{
+				return DateTime.Now.Month == DateOfBirth.Month
+					&& DateTime.Now.Day == DateOfBirth.Day;
+			}
+		}
+
+		// Constructor.
+		public Person(string name, DateTime dateOfBirth)
+		{
+			Name = name;
+			DateOfBirth = dateOfBirth;
+		}
+
+	}
+
+	// Customer is derived from Person.
+	// It has a Name, a DateOfBirth and a HasBirthday property.
+	// Additionally to the inherited properties, it also has a CustomerId.
+	class Customer : Person
+	{
+		public int CustomerId { get; }
+
+		// Customer constructor calls inherited Person constructor via ': base(...)' ...
+		public Customer(string name, DateTime dateOfBirth, int customerId)
+			: base(name, dateOfBirth)
+		{
+			// ... and initializes remaining members not inherited from base class
+			CustomerId = customerId;
+
+		}
+	}
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			int a = 5;
-			int b = 6;
-			int c = a * b;
+			Person x = new Person("Alice", new DateTime(1984, 2, 23));
+			Customer y = new Customer("Bob", new DateTime(1993, 5, 17), 999123);
 
-			try
-			{
-				Console.WriteLine("Hello");
-				throw new Exception("Something happened!");
-				Console.WriteLine("World");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("Error now this: " + e.Message);
-			}
-
+			Console.WriteLine("C_ID of y= {0}", y.CustomerId);
 			//Console.WriteLine("Hello World\n!" +x);
 		}
 	}
